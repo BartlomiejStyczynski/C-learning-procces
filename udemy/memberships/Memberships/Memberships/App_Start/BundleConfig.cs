9e17daa7d5727e2c183dd736cdfbafc0d7005711
BartlomiejStyczynski <71352679+BartlomiejStyczynski@users.noreply.github.com>
2020-11-08 10:04:15 +0000

    admin menu

--- udemy/memberships/Memberships/Memberships/App_Start/BundleConfig.cs
+++ udemy/memberships/Memberships/Memberships/App_Start/BundleConfig.cs
@@ -20,11 +20,14 @@ namespace Memberships
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js", "~/Scripts/respond.css"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/Admin").Include(
+                      "~/Scripts/AdminMenu.js"));
         }
     }
 }
