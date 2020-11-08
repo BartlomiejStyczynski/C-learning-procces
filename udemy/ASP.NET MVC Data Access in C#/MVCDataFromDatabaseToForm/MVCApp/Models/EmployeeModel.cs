03b820397b10550c80ea214395c84d09c4ef42df
BartlomiejStyczynski <71352679+BartlomiejStyczynski@users.noreply.github.com>
2020-11-08 16:44:15 +0000

    Data Access

---
+++ udemy/ASP.NET MVC Data Access in C#/MVCDataFromDatabaseToForm/MVCApp/Models/EmployeeModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services.Protocols;
+
+namespace MVCApp.Models
+{
+    public class EmployeeModel
+    {
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string ConfirmEmail { get; set; }
+        public string Password  { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
--- udemy/Memberships/Memberships/Memberships/Scripts/AdminMenu.js
+++
@@ -1,6 +0,0 @@
-﻿$(function ()
-{
-    $('[data-admin-menu]').hover(function () {
-        $('[data-admin-menu]').toggleClass('open')
-    })
-});
