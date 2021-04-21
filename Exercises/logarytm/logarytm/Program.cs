5e997ef349826c6b3fd5761907cc0b933c54a537
BartlomiejStyczynski <71352679+BartlomiejStyczynski@users.noreply.github.com>
2021-04-21 21:40:29 +0100

    VALIDATE FORM CODE SNIPPET
    add data annotations in model when validate sent form //

---
+++ Exercises/logarytm/logarytm/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace logarytm
+{
+    class Program
+
+    {
+
+        public class logus
+        {
+            public double ID { get; set; }
+            public double Wyni { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            
+            var kust = new List<logus>();
+            for (int i = 0; i < 1500; i++)
+            {
+                var nie = Math.Log2(615 + (i * i));
+                if (nie%1 == 0)
+                    Console.WriteLine(i); ;
+            }
+            foreach (var item in kust)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
